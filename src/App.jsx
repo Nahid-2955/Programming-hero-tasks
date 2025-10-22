4332ad8371c695303e10771828d3a8afa824e3df
TROJAN <moh.nahidmia@gmail.com>
2025-10-22 13:54:56 +0600

    finaly done

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ const [resolvedTasks, setResolvedTasks] = useState([])
   
   return (
     <>
-     <div className='nav_head flex justify-between max-w-400 mx-auto my-7'>
-        <div><p className='font-bold text-xl md:text-2xl'>CS-Ticket System</p></div>
+     <div className='nav_head flex justify-evenly max-w-auto mx-auto my-7'>
+        <div><p className='font-bold text-md md:text-2xl'>CS-Ticket System</p></div>
         <div>
           <ul className='flex gap-8 items-center list'>
             <li>Home</li>
@@ -66,37 +66,38 @@ const [resolvedTasks, setResolvedTasks] = useState([])
       </div>
 
      </div>
-     <div className='flex bg-black text-white justify-around mt-5 pb-15'>
-      <div className='mt-10 ml-10'><p className='text-xl font-bold mb-5'>CS Ticket System</p>
-      <p>Lorem Ipsum is simply dummy text of the <br /> printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard <br /> dummy text ever since the 1500s, when an <br /> unknown printer took a galley of type and <br /> scrambled it to make a type specimen book.</p></div>
-      <div className='mt-10'><p className='text-xl font-bold mb-5'>Company</p>
-      <ul className='gap-5'>
-        <li>About us</li>
-        <li>Our mission</li>
-        <li>Contact Saled</li>  
-      </ul></div>
-      <div className='mt-10'><p className='text-xl font-bold mb-5'>Services</p>
-      <ul className='gap-5'>
-        <li>Products and Services</li>
-        <li>Customer Stories</li>
-        <li>Download Apps</li>
-      </ul></div>
-      <div className='mt-10'><p className='text-xl font-bold mb-5'>Information</p>
-      <ul className='gap-5'>
-        <li>Privacy Policy</li>
-        <li>Terms and Condition</li>
-        <li>Join us</li>
-      </ul></div>
-      <div className='mt-10 mr-10'>
-        <ul>
-          <li><img src="./assets/x-twitter-logo-on-black-circle-5694247.png" alt="Logo of twitter X" /> @CS-Ticket System</li>
-          <li><img src="./assets/isolated-linkedin-logo-vector-illustration-icon-minsk-belarus-march-142909970.webp" alt="logo of linkedin" /> @CS-Ticket System</li>
-          <li><img src="./assets/images.png" alt="logo of facebook" /> @CS-Ticket System</li>
-          <li><img src="./assets/images2.png" alt="logo of email" /> @CS-Ticket System</li>
-        </ul>
-      </div>
+     <div className='flex bg-black text-white justify-around mt-5 pb-15 footer_section'>
+        <div className='mt-10 ml-10'>
+        <p className='text-xl font-bold mb-5'>CS Ticket System</p>
+        <p>Lorem Ipsum is simply dummy text of the <br /> printing and typesetting industry. Lorem <br /> Ipsum has been the industry's standard <br /> dummy text ever since the 1500s, when an <br /> unknown printer took a galley of type and <br /> scrambled it to make a type specimen book.</p></div>
+        <div className='mt-10'><p className='text-xl font-bold mb-5'>Company</p>
+        <ul className='gap-5'>
+          <li>About us</li>
+          <li>Our mission</li>
+          <li>Contact Saled</li>  
+        </ul></div>
+        <div className='mt-10'><p className='text-xl font-bold mb-5'>Services</p>
+        <ul className='gap-5'>
+          <li>Products and Services</li>
+          <li>Customer Stories</li>
+          <li>Download Apps</li>
+        </ul></div>
+        <div className='mt-10'><p className='text-xl font-bold mb-5'>Information</p>
+        <ul className='gap-5'>
+          <li>Privacy Policy</li>
+          <li>Terms and Condition</li>
+          <li>Join us</li>
+        </ul></div>
+        <div className='mt-10 mr-10'>
+          <ul>
+            <li><img src="./assets/x-twitter-logo-on-black-circle-5694247.png" alt="Logo of twitter X" /> @CS-Ticket System</li>
+            <li><img src="./assets/isolated-linkedin-logo-vector-illustration-icon-minsk-belarus-march-142909970.webp" alt="logo of linkedin" /> @CS-Ticket System</li>
+            <li><img src="./assets/images.png" alt="logo of facebook" /> @CS-Ticket System</li>
+            <li><img src="./assets/images2.png" alt="logo of email" /> @CS-Ticket System</li>
+          </ul>
+        </div>
      </div>
-     <div className='bg-black items-center text-white flex justify-center'>2025 CS-Ticket System. All Rights reserved.</div>
+     <div className='bg-black items-center text-white flex justify-center footer'>2025 CS-Ticket System. All Rights reserved.</div>
   <ToastContainer />
     </>
   )
